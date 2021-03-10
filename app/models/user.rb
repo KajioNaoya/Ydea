bc7262b2bf3ef9f7c346c7d191a43f5809e03bd2
naoya <kjo.hrd.eik@gmail.com>
2021-03-10 13:09:11 +0900

    ログ調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,9 +14,10 @@ class User < ApplicationRecord
   end
 
   def self.find_or_create_from_auth(auth)
-    logger.debug 'find or create 呼ばれたよ'
+    
     find_or_create_by(email: auth.info.email) do |user|
       user.password = Devise.friendly_token[0, 20]
+      logger.debug "auth.info.name = #{auth.info.name}"
       user.name = auth.info.name
       user.provider = auth.provider
       user.uid = auth.uid
